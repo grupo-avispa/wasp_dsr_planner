95137277451b967e0fc2ff91e3ce5d5b5008503f
Jose Galeas <jgaleas@uma.es>
2025-09-25 14:28:31 +0200

    New BT and action node "LangInvoke" for Physical_AI project
    Signed-off-by: Jose Galeas <jgaleas@uma.es>

---
+++ include/wasp_dsr_planner/executor/action/lang_invoke.hpp
@@ -0,0 +1,54 @@
+// Copyright (c) 2025 Grupo Avispa, DTE, Universidad de Málaga
+// Copyright (c) 2025 Jose M. Galeas Merchan
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef PLANNER_AGENT__EXECUTOR__EXECUTOR__ACTION__LANGINVOKE_HPP_
+#define PLANNER_AGENT__EXECUTOR__EXECUTOR__ACTION__LANGINVOKE_HPP_
+
+#include "behaviortree_cpp/behavior_tree.h"
+#include "dsr/api/dsr_api.h"
+
+#include "wasp_dsr_planner/executor/dsr_action.hpp"
+
+/**
+ * @class LangInvoke
+ * @brief This BT action node insert a DSR node of type 'lang_invoke' and
+ * invoke the language model to process the input text and generate a response.
+ */
+class LangInvoke : public DSRAction<lang_invoke_node_type>
+{
+public:
+  LangInvoke(
+    const std::string & xml_tag_name, const std::string & action_name,
+    const BT::NodeConfiguration & conf);
+
+  void getInputsOnTick() override;
+
+  bool setAttributesBeforeStart(DSR::Node & node) override;
+
+  bool setOutputsAfterFinished(DSR::Node & node) override;
+
+  static BT::PortsList providedPorts()
+  {
+    return providedBasicPorts({
+      BT::InputPort<std::string>("query", "User query to process"),
+      BT::OutputPort<std::string>("response", "Response from language model")
+    });
+  }
+
+private:
+  std::string query_;
+};
+
+#endif  // PLANNER_AGENT__EXECUTOR__EXECUTOR__ACTION__LANGINVOKE_HPP_
---
+++ src/executor/action/lang_invoke.cpp
@@ -0,0 +1,62 @@
+// Copyright (c) 2025 Grupo Avispa, DTE, Universidad de Málaga
+// Copyright (c) 2025 Jose M. Galeas Merchan
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <limits>
+
+#include "wasp_dsr_planner/executor/action/lang_invoke.hpp"
+
+LangInvoke::LangInvoke(
+  const std::string & xml_tag_name, const std::string & action_name,
+  const BT::NodeConfiguration & conf)
+: DSRAction<lang_invoke_node_type>(xml_tag_name, action_name, conf)
+{
+}
+
+void LangInvoke::getInputsOnTick()
+{
+  getInput<std::string>("query", query_);
+}
+
+bool LangInvoke::setAttributesBeforeStart(DSR::Node & node)
+{
+  bool success = false;
+  if (!query_.empty()) {
+    G_->add_or_modify_attrib_local<query_att>(node, query_);
+    success = true;
+  } else {
+    std::cout << "[" << action_name_ << "]: Query is not set" << std::endl;
+  }
+  return success;
+}
+
+bool LangInvoke::setOutputsAfterFinished(DSR::Node & node)
+{
+  bool success = false;
+  if (auto response = G_->get_attrib_by_name<response_att>(node); response.has_value()) {
+    setOutput<std::string>("response", response.value());
+    success = true;
+  }
+  return success;
+}
+
+#include "behaviortree_cpp/bt_factory.h"
+BT_REGISTER_NODES(factory) {
+  BT::NodeBuilder builder =
+    [](const std::string & name, const BT::NodeConfiguration & config) {
+      return std::make_unique<LangInvoke>(name, "lang_invoke", config);
+    };
+
+  factory.registerBuilder<LangInvoke>("LangInvoke", builder);
+}
